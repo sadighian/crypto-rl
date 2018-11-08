848ac74f2298bcc4b683e84f93e170ab1da42647
RedBanies3ofThem <jonathan.m.sadighian@gmail.com>
2018-11-08 08:00:14 +0100

    Fixed bug in logic
    Bitfinex Orderbook was not being reset after a websocket disconnection event.

--- bitfinex_connector/bitfinex_client.py
+++ bitfinex_connector/bitfinex_client.py
@@ -18,8 +18,7 @@ class BitfinexClient(Client):
 
             if self.book.new_tick(msg) is False:
                 self.retry_counter += 1
-                self.book.bids.warming_up = True
-                self.book.asks.warming_up = True
+                self.book.clear_book()
                 print('\n[Bitfinex - %s] ...going to try and reload the order book\n' % self.sym)
                 raise websockets.ConnectionClosed(1006, 'no reason')  # raise an exception to invoke reconnecting
 
