b9ee63aac12e861d01a747c6801a14b4d1fe1aee
RedBanies3ofThem <jonathan.m.sadighian@gmail.com>
2018-12-04 19:43:33 +0100

    Broker.py checkpoint
    Signed-off-by: RedBanies3ofThem <jonathan.m.sadighian@gmail.com>

--- trading_gym/broker.py
+++ trading_gym/broker.py
@@ -3,81 +3,70 @@ import pandas as pd
 
 class PositionI(object):
 
-    def __init__(self, max_position=1):
+    def __init__(self, side='buy', max_position=1):
         self.max_position_count = max_position
         self.positions = []
+        self.realized_pnl = 0.0
+        self.unrealized_pnl = 0.0
+        self.full_inventory = False
         self.position_count = 0
-        self.position_exposure = 0.
-        self.position_avg_price = 0.
-        self.realized_pnl = 0.
-        self._inventory_full = False
+        self.total_exposure = 0.0
+        self.side = side
 
     def add(self, order):
         if self.position_count < self.max_position_count:
             self.positions.append(order)
-            self._update(first_order_price=order['price'])
+            self.position_count += 1
+            self.total_exposure += order['price']
         # else:
         #     print('PositionI.add() have %i of %i positions open' % (self.position_count, self.max_position_count))
 
     def remove(self, order):
         if self.position_count > 0:
-            first_order = self.positions.pop()  # FIFO order inventory
-            self._update(first_order_price=first_order['price'], order=order)
+            opening_order = self.positions.pop()  # FIFO order inventory
+            self.position_count -= 1
+            self.total_exposure -= opening_order['price']
+            if self.side == 'buy':
+                self.realized_pnl += (order['price'] - opening_order['price']) / opening_order['price']
+            elif self.side == 'sell':
+                self.realized_pnl += (opening_order['price'] - order['price']) / opening_order['price']
+            else:
+                print('PositionI.remove() Warning - position side unrecognized = {}'.format(self.side))
         # else:
-        #     print('PositionI.remove() no position to remove')
+        #     print('PositionI.remove() no position in inventory to remove')
 
     def reset(self):
         self.positions.clear()
+        self.realized_pnl = 0.0
+        self.unrealized_pnl = 0.0
+        self.full_inventory = False
         self.position_count = 0
+        self.total_exposure = 0.0
 
-    def _update(self, first_order_price, order=None):
-        if order is not None:
-            if order['side'] == 'long':
-                realized_pnl = (order['price'] / first_order_price - 1.) #* 100.
-            elif order['side'] == 'short':
-                realized_pnl = (first_order_price / order['price'] - 1.) #* 100.
-            else:
-                realized_pnl = 0.
-                print('PositionI._update() Uknown order side for %s' % order)
+    def get_unrealized_pnl(self, midpoint=100.):
+        if self.position_count == 0:
+            return 0.0
 
-            self.realized_pnl += realized_pnl
+        average_price = self.total_exposure / self.position_count
+        pnl = 0.0
+        if self.side == 'buy':
+            pnl = (midpoint / average_price) - 1.0
+        elif self.side == 'sell':
+            pnl = (average_price / midpoint) - 1.0
+        else:
+            print('PositionI.remove() Warning - position side unrecognized = {}'.format(self.side))
 
-        self.position_exposure += first_order_price
-        self.position_count = len(self.positions)
-        self.position_avg_price = self.position_exposure / self.position_count if self.position_count > 0 else None
-
-        self._inventory_full = True if self.position_count == self.max_position_count else False
-
-    def get_unrealized_long_pnl(self, midpoint=100.):
-        pnl = 0.
-        for order in self.positions:
-            pnl += (midpoint / order['price'] - 1.) #* 100.
         return pnl
-
-    def get_unrealized_short_pnl(self, midpoint=100.):
-        pnl = 0.
-        for order in self.positions:
-            pnl += (order['price'] / midpoint - 1.) #* 100.
-        return pnl
-
-    @property
-    def full_inventory(self):
-        return self._inventory_full
 
 
 class Broker(object):
 
-    def __init__(self, ccy='BTC-USD', max_position=1):
-        self.long_inventory = PositionI(max_position=max_position)
-        self.short_inventory = PositionI(max_position=max_position)
-        self.sym = ccy
-        self.net_position_exposure = 0.0
-        self.deal_number = 0
-        self.transaction_details_columns = ['symbol', 'side', 'type', 'price', 'pnl', 'time', 'deal_number']
-        self.transaction_details = pd.DataFrame(columns=self.transaction_details_columns)
+    def __init__(self, max_position=1):
+        self.long_inventory = PositionI(side='buy', max_position=max_position)
+        self.short_inventory = PositionI(side='sell', max_position=max_position)
+        self.net_exposure = 0.0
 
     def add(self, order):
-        order['type'] = 'add'
         if order['side'] == 'long':
             self.long_inventory.add(order=order)
         elif order['side'] == 'short':
@@ -86,11 +75,9 @@ class Broker(object):
             print('Broker.add() unknown order.side = %s' % order)
             return
 
-        self.net_position_exposure = self.long_inventory.position_exposure - self.short_inventory.position_exposure
-        self._update_transaction_details(order=order)
+        self.net_exposure = self.long_inventory.total_exposure - self.short_inventory.total_exposure
 
     def remove(self, order):
-        order['type'] = 'remove'
         if order['side'] == 'long':
             self.long_inventory.remove(order=order)
         elif order['side'] == 'short':
@@ -99,26 +86,16 @@ class Broker(object):
             print('Broker.remove() unknown order.side = %s' % order['side'])
             return
 
-        self.net_position_exposure = self.long_inventory.position_exposure - self.short_inventory.position_exposure
-        self._update_transaction_details(order=order)
+        self.net_exposure = self.long_inventory.total_exposure - self.short_inventory.total_exposure
 
     def reset(self):
         self.long_inventory.reset()
         self.short_inventory.reset()
-        self.net_position_exposure = 0.
-        self.deal_number = 0
-        self.transaction_details = pd.DataFrame(columns=self.transaction_details_columns)
 
     def get_unrealized_pnl(self, midpoint=100.):
-        long_pnl = self.long_inventory.get_unrealized_long_pnl(midpoint=midpoint)
-        short_pnl = self.short_inventory.get_unrealized_short_pnl(midpoint=midpoint)
+        long_pnl = self.long_inventory.get_unrealized_pnl(midpoint=midpoint)
+        short_pnl = self.short_inventory.get_unrealized_pnl(midpoint=midpoint)
         return long_pnl + short_pnl
 
     def get_realized_pnl(self):
         return self.short_inventory.realized_pnl + self.long_inventory.realized_pnl
-
-    def _update_transaction_details(self, order):
-        order['deal_number'] = self.deal_number
-        order['symbol'] = self.sym
-        self.transaction_details.append(order, ignore_index=True)
-        self.deal_number += 1
--- trading_gym/trading_gym.py
+++ trading_gym/trading_gym.py
@@ -9,141 +9,136 @@ import logging
 import numpy as np
 import pandas as pd
 import os
-
-
-# DEFAULT_ACTION_SET = (
-#     (1, 0, 0, 0, 0),  # 0. Buy
-#     (0, 1, 0, 0, 0),  # 1. Close-buy
-#     (0, 0, 1, 0, 0),  # 2. Short
-#     (0, 0, 0, 1, 0),  # 3. Close-short
-#     (0, 0, 0, 0, 1),  # 4. Do nothing
-# )
-DEFAULT_ACTION_SET = {
-    0: 'buy',
-    1: 'close-buy',
-    2: 'short',
-    3: 'close-short',
-    4: 'no-action'
-}
-
-
-EXTRA_FEATURES = ['long_inventory', 'short_inventory']
+from sklearn.preprocessing import MinMaxScaler
 
 
 class TradingGym(Env):
 
-    def __init__(self, query, lags, training=True, env_id='coinbasepro-bitfinex-v0', step_size=1,
-                 fee=0.003, max_position=1):
+    instance_count = 0
+
+    def __init__(self, data,
+                 training=True, env_id='coinbasepro-bitfinex-v0',
+                 step_size=1, fee=0.003, max_position=1):
+
         # properties required for instantiation
         self.training = training
         self.env_id = env_id
         self.step_size = step_size
         self.fee = fee
         self.max_position = max_position
+        self.inventory_features = ['long_inventory', 'short_inventory']
+        self.scaler = MinMaxScaler()
 
         # properties that get reset()
-        self.reward = None
-        self.done = False
-        self.local_step_number = 0
+        self._reward = None
+        self._done = False
+        self._local_step_number = 0
+        self._state = None
+        self._next_state = None
+        # derive gym.env properties
+        self._actions = {
+            0: 'buy',
+            1: 'close-buy',
+            2: 'short',
+            3: 'close-short',
+            4: 'no-action'
+        }
+        self._midpoint = None
 
         # get historical data for simulations
-        self.broker = Broker(ccy=query['ccy'][0], max_position=max_position)
+        self.broker = Broker()
         self.sim = Sim()
-        self.features = self.sim.get_feature_labels(include_system_time=False, lags=lags)
-        self.midpoint_index = len(self.sim.get_feature_labels(include_system_time=False)) + 2
-        print('midpoint index is %i' % self.midpoint_index)
-        # self.data = self.sim.get_env_data(query=query,
-        #                                   coinbaseOrderBook=CoinbaseOrderBook(query['ccy'][0]),
-        #                                   bitfinexOrderBook=BitfinexOrderBook(query['ccy'][1]),
-        #                                   lags=lags)
-        self.data = pd.read_csv(os.getcwd() + '/env_data_history.csv')
-        print('...loaded data from csv.')
-        delete_columns = [self.data.columns[0], 't']
-        print('...deleting column: %s' % delete_columns)
-        del self.data[self.data.columns[0]]
-        del self.data['t']
+        self.features = self.sim.get_feature_labels(include_system_time=False, lags=0)
 
-        self.data = self.data.values
+        # load the data
+        self.data = data.values
         self.observation = self.reset()
 
-        # derive gym.env properties
-        self.actions = DEFAULT_ACTION_SET
-        self.action_space = spaces.Discrete(len(self.actions))
+        self.action_space = spaces.Discrete(len(self._actions))
         self.observation_space = spaces.Box(low=-np.inf,
                                             high=np.inf,
-                                            shape=(1, len(self.features) + len(EXTRA_FEATURES)),
+                                            shape=(1, len(self.features) + len(self.inventory_features)),
                                             dtype=np.float32)
-        self.env_id = env_id
 
         # logging
         logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
         self.logger = logging.getLogger(env_id)
         self.logger.info('Making new env: {}'.format(env_id))
+        TradingGym.instance_count += 1
 
     def step(self, action):
-        if self.done:
+        if self._done:
+            self.logger.info('{} is done.'.format(self.env_id))
             self.observation = self.reset()
-            return self.observation, self.reward, self.done, {'isDone': self.done}
+            return self.observation[1], self._reward, self._done, {}
 
-        self.observation = self.data[self.local_step_number]
-        self.reward = self.get_reward(action, self.observation)
-        self.observation = np.concatenate((self.observation, self.create_position_features()))
-        self.observation = self.observation.reshape(1, -1)
+        self._next_state = np.concatenate((self.process_data(self.data[self._local_step_number]),
+                                           self.create_position_features()))
+        self.observation = (self._state, self._next_state)
+        self._state = self._next_state
 
-        self.local_step_number += self.step_size
-        if self.local_step_number > self.data.shape[0] - 2:
-            self.done = True
+        self._midpoint = self.data[self._local_step_number][0]
+        self._reward = self.get_reward(action)
 
-        return self.observation, self.reward, self.done, {'local_step_number': self.local_step_number}
+        self._local_step_number += self.step_size
+        if self._local_step_number > self.data.shape[0] - 2:
+            self._done = True
+
+        return self.observation[1], self._reward, self._done, {}
 
     def reset(self):
-        self.reward = None
-        self.done = False
+        print('{} has reset'.format(self.env_name))
+        self._reward = None
+        self._done = False
         self.broker.reset()
-        self.local_step_number = random.randint(0, self.data.shape[0] - int(self.data.shape[0] * .2)) \
-            if self.training else 0
-        self.observation = np.concatenate((self.data[self.local_step_number], self.create_position_features()))
-        self.observation = self.observation.reshape(1, -1)
-        self.local_step_number += 1
+        self._local_step_number = 0
+        self._state = np.concatenate((self.process_data(self.data[self._local_step_number]),
+                                      self.create_position_features()))
+        self._local_step_number += 1
+        self._next_state = np.concatenate((self.process_data(self.data[self._local_step_number]),
+                                           self.create_position_features()))
+        self._local_step_number += 1
+        self.observation = (self._state, self._next_state)
         return self.observation
 
     def render(self, mode='human'):
         return None
 
     def close(self):
+        self.data = None
+        self.broker = None
+        self.sim = None
         return
 
     def seed(self, seed=None):
         np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def get_reward(self, action, observation):
-        midpoint = observation[self.midpoint_index]
-
+    def get_reward(self, action):
         if action == 0:  # buy
             order = {
-                'price': midpoint + (midpoint * self.fee),
+                'price': self._midpoint + (self._midpoint * self.fee),
                 'size': 10000.0,
                 'side': 'long'
             }
             self.broker.add(order=order)
         elif action == 1:  # close-buy
             order = {
-                'price': midpoint,
+                'price': self._midpoint,
                 'size': 10000.0,
                 'side': 'long'
             }
             self.broker.remove(order=order)
         elif action == 2:  # short
             order = {
-                'price': midpoint - (midpoint * self.fee),
+                'price': self._midpoint - (self._midpoint * self.fee),
                 'size': 10000.0,
                 'side': 'short'
             }
             self.broker.add(order=order)
         elif action == 3:  # cover-short
             order = {
-                'price': midpoint,
+                'price': self._midpoint,
                 'size': 10000.0,
                 'side': 'short'
             }
@@ -151,13 +146,43 @@ class TradingGym(Env):
         elif action == 4:  # do nothing
             pass
         else:
-            print('Unknown action to take in get_reward(): action={} | midpoint={}'.format(action, midpoint))
+            print('Unknown action to take in get_reward(): action={} | midpoint={}'.format(action, self._midpoint))
 
-        unrealized_pnl = self.broker.get_unrealized_pnl(midpoint=midpoint)
-        realized_pnl = self.broker.get_realized_pnl()
-        #  TODO: Add penalty for not having an empty inventory & short borrowing fee
-        return unrealized_pnl + realized_pnl
+        # unrealized_pnl = self.broker.get_unrealized_pnl(midpoint=self._midpoint)
+        # realized_pnl = self.broker.get_realized_pnl()
+        # penalty = self._calculate_penalty(action=action)
+        # total_pnl = unrealized_pnl + realized_pnl - penalty
+        #
+        # _reward = total_pnl - self.prev_total_pnl
+        # self.prev_total_pnl = total_pnl + penalty
+        return None
+
+    def _calculate_penalty(self, action=0, penalty=0.0):
+        _penalty = penalty
+        is_long_inventory_full = self.broker.long_inventory.full_inventory
+        is_short_inventory_full = self.broker.short_inventory.full_inventory
+
+        if action == 0:  # buy
+            if is_long_inventory_full:
+                _penalty += self.fee
+        elif action == 1:  # close-buy
+            pass
+        elif action == 2:  # short
+            if is_short_inventory_full:
+                _penalty += self.fee
+        elif action == 3:  # cover-short
+            pass
+        elif action == 4:  # do nothing
+            pass
+        else:
+            print('Unknown action to take in get_reward(): action={} | midpoint={}'
+                  .format(action, self._midpoint))
+
+        return _penalty
 
     def create_position_features(self):
-        return self.broker.long_inventory.position_count / self.max_position, \
-               self.broker.short_inventory.position_count / self.max_position
+        return np.array((self.broker.long_inventory.position_count / self.max_position,
+                         self.broker.short_inventory.position_count / self.max_position))
+
+    def process_data(self, _next_state):
+        return self.scaler.transform(_next_state.reshape(1, -1)).reshape(_next_state.shape)
