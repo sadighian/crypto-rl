f789b8e3a7838363c6c1900f34ed2319ea89abdb
RedBanies3ofThem <jonathan.m.sadighian@gmail.com>
2019-09-17 16:29:19 +0200

    Refactor Checkpoint: Created and implemented base classes for environments

--- gym_trading/envs/market_maker.py
+++ gym_trading/envs/market_maker.py
@@ -1,11 +1,7 @@
-from gym import Env, spaces
-from data_recorder.database.simulator import Simulator as Sim
+from gym import spaces
 from gym_trading.utils.mm_broker import Broker, Order
-from gym_trading.utils.render_env import TradingGraph
-from configurations.configs import INDICATOR_WINDOW, INDICATOR_WINDOW_MAX
-from gym_trading.indicators import RSI
-from gym_trading.indicators import TnS
-from gym_trading.indicators.indicator import IndicatorManager
+from configurations.configs import INDICATOR_WINDOW_MAX
+from gym_trading.envs.base_env import BaseEnvironment
 import logging
 import numpy as np
 
@@ -15,23 +11,8 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
 logger = logging.getLogger('MarketMaker')
 
 
-class MarketMaker(Env):
-    # gym.env required
-    metadata = {'render.modes': ['human']}
+class MarketMaker(BaseEnvironment):
     id = 'market-maker-v0'
-
-    # Turn to true if Bitifinex is in the dataset (e.g., include_bitfinex=True)
-    features = Sim.get_feature_labels(include_system_time=False,
-                                      include_bitfinex=False)
-    best_bid_index = features.index('coinbase_bid_distance_0')
-    best_ask_index = features.index('coinbase_ask_distance_0')
-    notional_bid_index = features.index('coinbase_bid_notional_0')
-    notional_ask_index = features.index('coinbase_ask_notional_0')
-
-    buy_trade_index = features.index('coinbase_buys')
-    sell_trade_index = features.index('coinbase_sells')
-
-    target_pnl = 0.03  # 3.0% gain per episode (i.e., day)
 
     def __init__(self, *,
                  fitting_file='LTC-USD_2019-04-07.csv.xz',
@@ -44,57 +25,22 @@ class MarketMaker(Env):
                  training=True,
                  format_3d=False,
                  z_score=True):
-
-        # properties required for instantiation
-        self.action_repeats = action_repeats
-        self._seed = seed
-        self._random_state = np.random.RandomState(seed=self._seed)
-        self.training = training
-        self.step_size = step_size
-        self.max_position = max_position
-        self.window_size = window_size
-        self.format_3d = format_3d  # e.g., [window, features, *NEW_AXIS*]
-
-        self.action = 0
-        # derive gym.env properties
+        super(MarketMaker, self).__init__(
+            fitting_file=fitting_file,
+            testing_file=testing_file,
+            step_size=step_size,
+            max_position=max_position,
+            window_size=window_size,
+            seed=seed,
+            action_repeats=action_repeats,
+            training=training,
+            format_3d=format_3d,
+            z_score=z_score
+        )
         self.actions = np.eye(17, dtype=np.float32)
-
-        self.sym = testing_file[:7]  # slice the CCY from the filename
-
-        # properties that get reset()
-        self.reward = 0.0
-        self.done = False
-        self.local_step_number = 0
-        self.midpoint = 0.0
-        self.observation = None
 
         # get Broker class to keep track of PnL and orders
         self.broker = Broker(max_position=max_position)
-        # get historical data for simulations
-        self.sim = Sim(use_arctic=False, z_score=z_score)
-
-        self.prices_, self.data, self.normalized_data = self.sim.load_environment_data(
-            fitting_file, testing_file)
-
-        self.max_steps = self.data.shape[0] - self.step_size * \
-            self.action_repeats - 1
-
-        # load indicators into the indicator manager
-        self.tns = IndicatorManager()
-        self.rsi = IndicatorManager()
-        for window in INDICATOR_WINDOW:
-            self.tns.add(('tns_{}'.format(window), TnS(window=window)))
-            self.rsi.add(('rsi_{}'.format(window), RSI(window=window)))
-
-        # rendering class
-        self._render = TradingGraph(sym=self.sym)
-
-        # graph midpoint prices
-        self._render.reset_render_data(
-            y_vec=self.prices_[:np.shape(self._render.x_vec)[0]])
-
-        # buffer for appending lags
-        self.data_buffer = list()
 
         self.action_space = spaces.Discrete(len(self.actions))
         self.reset()  # reset to load observation.shape
@@ -109,129 +55,30 @@ class MarketMaker(Env):
     def __str__(self):
         return '{} | {}-{}'.format(MarketMaker.id, self.sym, self._seed)
 
-    def step(self, action: int):
-        for current_step in range(self.action_repeats):
+    def get_step_reward(self, **kwargs):
+        self.broker.step(bid_price=step_best_bid, ask_price=step_best_ask,
+            buy_volume=buy_volume, sell_volume=sell_volume, step=self.local_step_number)
 
-            if self.done:
-                self.reset()
-                return self.observation, self.reward, self.done
-
-            # reset the reward if there ARE action repeats
-            if current_step == 0:
-                self.reward = 0.
-                step_action = action
-            else:
-                step_action = 0
-
-            # Get current step's midpoint
-            self.midpoint = self.prices_[self.local_step_number]
-            # Pass current time step midpoint to broker to calculate PnL,
-            # or if any open orders are to be filled
-            step_best_bid, step_best_ask = self._get_nbbo()
-            buy_volume = self._get_book_data(MarketMaker.buy_trade_index)
-            sell_volume = self._get_book_data(MarketMaker.sell_trade_index)
-
-            self.tns.step(buys=buy_volume, sells=sell_volume)
-            self.rsi.step(price=self.midpoint)
-
-            step_reward = self.broker.step(
-                bid_price=step_best_bid,
-                ask_price=step_best_ask,
-                buy_volume=buy_volume,
-                sell_volume=sell_volume,
-                step=self.local_step_number)
-
-            self.reward += self._send_to_broker_and_get_reward(action=step_action)
-            self.reward += step_reward
-
-            step_observation = self._get_step_observation(action=action)
-            self.data_buffer.append(step_observation)
-
-            if len(self.data_buffer) > self.window_size:
-                del self.data_buffer[0]
-
-            self.local_step_number += self.step_size
-
-        self.observation = self._get_observation()
-
-        if self.local_step_number > self.max_steps:
-            self.done = True
-            self.reward += self.broker.flatten_inventory(*self._get_nbbo())
-
-        return self.observation, self.reward, self.done, {}
-
-    def reset(self):
-        if self.training:
-            self.local_step_number = self._random_state.randint(
-                low=1,
-                high=self.data.shape[0] // 4)
-        else:
-            self.local_step_number = 0
-
-        msg = ' {}-{} reset. Episode pnl: {:.4f} with {} trades | First step: {}'.format(
-            self.sym, self._seed,
-            self.broker.get_total_pnl(midpoint=self.midpoint),
-            self.broker.get_total_trade_count(),
-            self.local_step_number)
-        logger.info(msg)
-
-        self.reward = 0.0
-        self.done = False
-        self.broker.reset()
-        self.data_buffer.clear()
-        self.rsi.reset()
-        self.tns.reset()
-
-        for step in range(self.window_size + INDICATOR_WINDOW_MAX):
-            self.midpoint = self.prices_[self.local_step_number]
-
-            step_buy_volume = self._get_book_data(MarketMaker.buy_trade_index)
-            step_sell_volume = self._get_book_data(MarketMaker.sell_trade_index)
-            self.tns.step(buys=step_buy_volume, sells=step_sell_volume)
-            self.rsi.step(price=self.midpoint)
-
-            step_observation = self._get_step_observation(action=0)
-            self.data_buffer.append(step_observation)
-
-            self.local_step_number += self.step_size
-            if len(self.data_buffer) > self.window_size:
-                del self.data_buffer[0]
-
-        self.observation = self._get_observation()
-
-        return self.observation
-
-    def render(self, mode='human'):
-        self._render.render(midpoint=self.midpoint, mode=mode)
-
-    def close(self):
-        logger.info('{}-{} is being closed.'.format(self.id, self.sym))
-        self.data = None
-        self.normalized_data = None
-        self.prices_ = None
-        self.broker = None
-        self.sim = None
-        self.data_buffer = None
-        self.tns = None
-        self.rsi = None
-        return
-
-    def seed(self, seed=1):
-        self._random_state = np.random.RandomState(seed=seed)
-        self._seed = seed
-        logger.info('Setting seed in MarketMaker.seed({})'.format(seed))
-        return [seed]
-
-    @staticmethod
-    def _process_data(_next_state):
+    def _create_position_features(self):
         """
-        Reshape observation and clip outliers (values +/- 10)
-        :param _next_state: observation space
-        :return: (np.array) clipped observation space
+        Create an array with features related to the agent's inventory
+        :return: (np.array) normalized position features
         """
-        return np.clip(_next_state.reshape((1, -1)), -10., 10.)
+        return np.array(
+            (self.broker.long_inventory.position_count / self.max_position,
+             self.broker.short_inventory.position_count / self.max_position,
+             self.broker.get_total_pnl(midpoint=self.midpoint) /
+             BaseEnvironment.target_pnl,
+             self.broker.long_inventory.get_unrealized_pnl(self.midpoint) /
+             self.broker.reward_scale,
+             self.broker.short_inventory.get_unrealized_pnl(self.midpoint) /
+             self.broker.reward_scale,
+             self.broker.get_long_order_distance_to_midpoint(midpoint=self.midpoint),
+             self.broker.get_short_order_distance_to_midpoint(midpoint=self.midpoint),
+             *self.broker.get_queues_ahead_features()),
+            dtype=np.float32)
 
-    def _send_to_broker_and_get_reward(self, action: int):
+    def _send_to_broker(self, action: int):
         """
         Create or adjust orders per a specified action and adjust for penalties.
         :param action: (int) current step's action
@@ -339,41 +186,6 @@ class MarketMaker(Env):
 
         return reward
 
-    def _create_position_features(self):
-        """
-        Create an array with features related to the agent's inventory
-        :return: (np.array) normalized position features
-        """
-        return np.array(
-            (self.broker.long_inventory.position_count / self.max_position,
-             self.broker.short_inventory.position_count / self.max_position,
-             self.broker.get_total_pnl(midpoint=self.midpoint) /
-             MarketMaker.target_pnl,
-             self.broker.long_inventory.get_unrealized_pnl(self.midpoint) /
-             self.broker.reward_scale,
-             self.broker.short_inventory.get_unrealized_pnl(self.midpoint) /
-             self.broker.reward_scale,
-             self.broker.get_long_order_distance_to_midpoint(midpoint=self.midpoint),
-             self.broker.get_short_order_distance_to_midpoint(midpoint=self.midpoint),
-             *self.broker.get_queues_ahead_features()),
-            dtype=np.float32)
-
-    def _create_action_features(self, action):
-        """
-        Create a features array for the current time step's action.
-        :param action: (int) action number
-        :return: (np.array) One-hot of current action
-        """
-        return self.actions[action]
-
-    def _create_indicator_features(self):
-        """
-        Create features vector with environment indicators.
-        :return: (np.array) Indicator values for current time step
-        """
-        return np.array((*self.tns.get_value(), *self.rsi.get_value()),
-                        dtype=np.float32)
-
     def _create_order_at_level(self, reward: float, discouragement: float,
                                level=0, side='long'):
         """
@@ -445,52 +257,3 @@ class MarketMaker(Env):
                 reward += discouragement
 
         return reward
-
-    def _get_nbbo(self):
-        """
-        Get best bid and offer
-        :return: (tuple) best bid and offer
-        """
-        best_bid = round(self.midpoint - self._get_book_data(
-            MarketMaker.best_bid_index), 2)
-        best_ask = round(self.midpoint + self._get_book_data(
-            MarketMaker.best_ask_index), 2)
-        return best_bid, best_ask
-
-    def _get_book_data(self, index=0):
-        """
-        Return step 'n' of order book snapshot data
-        :param index: (int) step 'n' to look up in order book snapshot history
-        :return: (np.array) order book snapshot vector
-        """
-        return self.data[self.local_step_number][index]
-
-    def _get_step_observation(self, action=0):
-        """
-        Current step observation, NOT including historical data.
-        :param action: (int) current step action
-        :return: (np.array) Current step observation
-        """
-        step_position_features = self._create_position_features()
-        step_action_features = self._create_action_features(action=action)
-        step_indicator_features = self._create_indicator_features()
-        return np.concatenate(
-            (self._process_data(self.normalized_data[self.local_step_number]),
-             step_indicator_features,
-             step_position_features,
-             step_action_features,
-             np.array([self.reward])),
-            axis=None)
-
-    def _get_observation(self):
-        """
-        Current step observation, including historical data.
-
-        If format_3d is TRUE: Expand the observation space from 2 to 3 dimensions.
-        (note: This is necessary for conv nets in Baselines.)
-        :return: (np.array) Observation state for current time step
-        """
-        observation = np.array(self.data_buffer, dtype=np.float32)
-        if self.format_3d:
-            observation = np.expand_dims(observation, axis=-1)
-        return observation
--- gym_trading/envs/price_jump.py
+++ gym_trading/envs/price_jump.py
@@ -1,14 +1,10 @@
-from gym import Env, spaces
-from data_recorder.database.simulator import Simulator as Sim
+from gym import spaces
 from gym_trading.utils.broker import Broker, Order
-from gym_trading.utils.render_env import TradingGraph
-from configurations.configs import MARKET_ORDER_FEE, INDICATOR_WINDOW, \
-    INDICATOR_WINDOW_MAX
-from gym_trading.indicators import RSI
-from gym_trading.indicators import TnS
-from gym_trading.indicators.indicator import IndicatorManager
+from configurations.configs import INDICATOR_WINDOW_MAX
 import logging
 import numpy as np
+
+from gym_trading.envs.base_env import BaseEnvironment
 
 
 # logging
@@ -16,23 +12,8 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
 logger = logging.getLogger('PriceJump')
 
 
-class PriceJump(Env):
-
-    metadata = {'render.modes': ['human']}
+class PriceJump(BaseEnvironment):
     id = 'long-short-v0'
-    # Turn to true if Bitifinex is in the dataset (e.g., include_bitfinex=True)
-    features = Sim.get_feature_labels(include_system_time=False,
-                                      include_bitfinex=False)
-    best_bid_index = features.index('coinbase_bid_distance_0')
-    best_ask_index = features.index('coinbase_ask_distance_0')
-    notional_bid_index = features.index('coinbase_bid_notional_0')
-    notional_ask_index = features.index('coinbase_ask_notional_0')
-
-    buy_trade_index = features.index('coinbase_buys')
-    sell_trade_index = features.index('coinbase_sells')
-
-    target_pnl = 0.03  # 3.0% gain per episode (i.e., day)
-    fee = MARKET_ORDER_FEE
 
     def __init__(self, *,
                  fitting_file='LTC-USD_2019-04-07.csv.xz',
@@ -45,57 +26,25 @@ class PriceJump(Env):
                  training=True,
                  format_3d=False,
                  z_score=True):
-
-        # properties required for instantiation
-        self.action_repeats = action_repeats
-        self._seed = seed
-        self._random_state = np.random.RandomState(seed=self._seed)
-        self.training = training
-        self.step_size = step_size
-        self.max_position = max_position
-        self.window_size = window_size
-        self.format_3d = format_3d  # e.g., [window, features, *NEW_AXIS*]
+        super(PriceJump, self).__init__(
+            fitting_file=fitting_file,
+            testing_file=testing_file,
+            step_size=step_size,
+            max_position=max_position,
+            window_size=window_size,
+            seed=seed,
+            action_repeats=action_repeats,
+            training=training,
+            format_3d=format_3d,
+            z_score=z_score
+        )
 
         self.action = 0
         # derive gym.env properties
         self.actions = np.eye(3, dtype=np.float32)
 
-        self.sym = testing_file[:7]  # slice the CCY from the filename
-
-        # properties that get reset()
-        self.reward = 0.0
-        self.done = False
-        self.local_step_number = 0
-        self.midpoint = 0.0
-        self.observation = None
-
         # get Broker class to keep track of PnL and orders
         self.broker = Broker(max_position=max_position)
-        # get historical data for simulations
-        self.sim = Sim(use_arctic=False, z_score=z_score)
-
-        self.prices_, self.data, self.normalized_data = self.sim.load_environment_data(
-            fitting_file, testing_file)
-
-        self.max_steps = self.data.shape[0] - self.step_size * \
-            self.action_repeats - 1
-
-        # load indicators into the indicator manager
-        self.tns = IndicatorManager()
-        self.rsi = IndicatorManager()
-        for window in INDICATOR_WINDOW:
-            self.tns.add(('tns_{}'.format(window), TnS(window=window)))
-            self.rsi.add(('rsi_{}'.format(window), RSI(window=window)))
-
-        # rendering class
-        self._render = TradingGraph(sym=self.sym)
-
-        # graph midpoint prices
-        self._render.reset_render_data(
-            y_vec=self.prices_[:np.shape(self._render.x_vec)[0]])
-
-        # buffer for appending lags
-        self.data_buffer = list()
 
         self.action_space = spaces.Discrete(len(self.actions))
         self.reset()  # reset to load observation.shape
@@ -136,7 +85,7 @@ class PriceJump(Env):
 
             self.broker.step(midpoint=self.midpoint)
 
-            self.reward += self._send_to_broker_and_get_reward(action=step_action)
+            self.reward += self._send_to_broker(action=step_action)
 
             step_observation = self._get_step_observation(action=action)
             self.data_buffer.append(step_observation)
@@ -156,78 +105,7 @@ class PriceJump(Env):
 
         return self.observation, self.reward, self.done, {}
 
-    def reset(self):
-        if self.training:
-            self.local_step_number = self._random_state.randint(
-                low=1,
-                high=self.data.shape[0] // 4)
-        else:
-            self.local_step_number = 0
-
-        msg = ' {}-{} reset. Episode pnl: {:.4f} with {} trades. First step: {}'.format(
-            self.sym, self._seed,
-            self.broker.get_total_pnl(midpoint=self.midpoint),
-            self.broker.get_total_trade_count(),
-            self.local_step_number)
-        logger.info(msg)
-
-        self.reward = 0.0
-        self.done = False
-        self.broker.reset()
-        self.data_buffer.clear()
-        self.rsi.reset()
-        self.tns.reset()
-
-        for step in range(self.window_size + INDICATOR_WINDOW_MAX):
-            self.midpoint = self.prices_[self.local_step_number]
-
-            step_buy_volume = self._get_book_data(PriceJump.buy_trade_index)
-            step_sell_volume = self._get_book_data(PriceJump.sell_trade_index)
-            self.tns.step(buys=step_buy_volume, sells=step_sell_volume)
-            self.rsi.step(price=self.midpoint)
-
-            step_observation = self._get_step_observation(action=0)
-            self.data_buffer.append(step_observation)
-
-            self.local_step_number += self.step_size
-            if len(self.data_buffer) > self.window_size:
-                del self.data_buffer[0]
-
-        self.observation = self._get_observation()
-
-        return self.observation
-
-    def render(self, mode='human'):
-        self._render.render(midpoint=self.midpoint, mode=mode)
-
-    def close(self):
-        logger.info('{}-{} is being closed.'.format(self.id, self.sym))
-        self.data = None
-        self.normalized_data = None
-        self.prices_ = None
-        self.broker = None
-        self.sim = None
-        self.data_buffer = None
-        self.tns = None
-        self.rsi = None
-        return
-
-    def seed(self, seed=1):
-        self._random_state = np.random.RandomState(seed=seed)
-        self._seed = seed
-        logger.info('Setting seed in PriceJump.seed({})'.format(seed))
-        return [seed]
-
-    @staticmethod
-    def _process_data(_next_state):
-        """
-        Reshape observation and clip outliers (values +/- 10)
-        :param _next_state: observation space
-        :return: (np.array) clipped observation space
-        """
-        return np.clip(_next_state.reshape((1, -1)), -10., 10.)
-
-    def _send_to_broker_and_get_reward(self, action: int):
+    def _send_to_broker(self, action: int):
         """
         Create or adjust orders per a specified action and adjust for penalties.
         :param action: (int) current step's action
@@ -240,10 +118,10 @@ class PriceJump(Env):
             reward += discouragement
 
         elif action == 1:  # buy
-            price_fee_adjusted = self.midpoint + (PriceJump.fee * self.midpoint)
+            # price_fee_adjusted = self.midpoint + (PriceJump.fee * self.midpoint)
             if self.broker.short_inventory_count > 0:
                 order = Order(ccy=self.sym, side='short',
-                              price=price_fee_adjusted,
+                              price=self.midpoint,  #price_fee_adjusted,
                               step=self.local_step_number)
                 self.broker.remove(order=order)
                 reward += self.broker.get_reward(side=order.side) / \
@@ -251,7 +129,7 @@ class PriceJump(Env):
 
             elif self.broker.long_inventory_count >= 0:
                 order = Order(ccy=self.sym, side='long',
-                              price=price_fee_adjusted,
+                              price=self.midpoint,  #price_fee_adjusted,
                               step=self.local_step_number)
                 if self.broker.add(order=order) is False:
                     reward -= discouragement
@@ -262,17 +140,17 @@ class PriceJump(Env):
                              'unable to place an order with broker').format(action))
 
         elif action == 2:  # sell
-            price_fee_adjusted = self.midpoint - (PriceJump.fee * self.midpoint)
+            # price_fee_adjusted = self.midpoint - (PriceJump.fee * self.midpoint)
             if self.broker.long_inventory_count > 0:
                 order = Order(ccy=self.sym, side='long',
-                              price=price_fee_adjusted,
+                              price=self.midpoint,  #price_fee_adjusted,
                               step=self.local_step_number)
                 self.broker.remove(order=order)
                 reward += self.broker.get_reward(side=order.side) / \
                     self.broker.reward_scale  # scale realized PnL
             elif self.broker.short_inventory_count >= 0:
                 order = Order(ccy=self.sym, side='short',
-                              price=price_fee_adjusted,
+                              price=self.midpoint,  #price_fee_adjusted,
                               step=self.local_step_number)
                 if self.broker.add(order=order) is False:
                     reward -= discouragement
@@ -302,68 +180,3 @@ class PriceJump(Env):
              self.broker.short_inventory.get_unrealized_pnl(self.midpoint) /
                 self.broker.reward_scale),
             dtype=np.float32)
-
-    def _create_action_features(self, action):
-        """
-        Create a features array for the current time step's action.
-        :param action: (int) action number
-        :return: (np.array) One-hot of current action
-        """
-        return self.actions[action]
-
-    def _create_indicator_features(self):
-        """
-        Create features vector with environment indicators.
-        :return: (np.array) Indicator values for current time step
-        """
-        return np.array((*self.tns.get_value(), *self.rsi.get_value()),
-                        dtype=np.float32)
-
-    def _get_nbbo(self):
-        """
-        Get best bid and offer
-        :return: (tuple) best bid and offer
-        """
-        best_bid = round(self.midpoint - self._get_book_data(
-            PriceJump.best_bid_index), 2)
-        best_ask = round(self.midpoint + self._get_book_data(
-            PriceJump.best_ask_index), 2)
-        return best_bid, best_ask
-
-    def _get_book_data(self, index=0):
-        """
-        Return step 'n' of order book snapshot data
-        :param index: (int) step 'n' to look up in order book snapshot history
-        :return: (np.array) order book snapshot vector
-        """
-        return self.data[self.local_step_number][index]
-
-    def _get_step_observation(self, action=0):
-        """
-        Current step observation, NOT including historical data.
-        :param action: (int) current step action
-        :return: (np.array) Current step observation
-        """
-        step_position_features = self._create_position_features()
-        step_action_features = self._create_action_features(action=action)
-        step_indicator_features = self._create_indicator_features()
-        return np.concatenate(
-            (self._process_data(self.normalized_data[self.local_step_number]),
-             step_indicator_features,
-             step_position_features,
-             step_action_features,
-             np.array([self.reward])),
-            axis=None)
-
-    def _get_observation(self):
-        """
-        Current step observation, including historical data.
-
-        If format_3d is TRUE: Expand the observation space from 2 to 3 dimensions.
-        (note: This is necessary for conv nets in Baselines.)
-        :return: (np.array) Observation state for current time step
-        """
-        observation = np.array(self.data_buffer, dtype=np.float32)
-        if self.format_3d:
-            observation = np.expand_dims(observation, axis=-1)
-        return observation
